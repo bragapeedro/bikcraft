fa2ef339620215ef255f2a23e4e375007c4347d8
Pedro Braga <bragapeedro@gmail.com>
2022-03-29 10:25:32 -0400

    Add galery interaction

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links de navegação
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,50 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//Ativar itens do orçamento dinamicamente
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametros) {
+  const elemento = document.getElementById(parametros);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+//Expandir perguntas
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+//Galeria de Bikes
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 920px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
